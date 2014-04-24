cf7a2d2c1cda4a6e00804c00f41f24babee3a2a7
ZitRo <zitros.lab@gmail.com>
2014-04-25 00:20:28 +0300

    unused file remove

--- config.js
+++ config.js
@@ -7,14 +7,14 @@ module.exports = {
         modulePath: "./node_modules/globalsDB/cache", // globalsDB path to node module file (without extension)
         databases: { // list of available database directories
             primaryDB: "C:/HardProgram/Globals/mgr",
-            secondaryDB: "C:/HardProgram/Globals/mgr2"
+            secondaryDB: "C:/HardProgram/Globals/mgr2" // @test
         }
     },
 
     server: { // server settings
         port: 57775, // port of server WebSocket application
         masterPassword: "protect", // second password, server will disconnect any client without masterPassword if on
-        useMasterPassword: false, // turn master password usage on
+        useMasterPassword: true, // turn master password usage on
         log: true, // log connections
         logFileName: "log.txt" // !RELATIVE! path with file name
     },
@@ -22,11 +22,11 @@ module.exports = {
     localClient: { // local client settings
         enabled: true, // use local HTTP client (for browser application)
         directory: "client", // !RELATIVE! frontend location to this file
-        port: 80 // port of client application. MAKE SURE THAT CLIENT PORT DOES NOT MATCH SERVER PORT
+        port: 80 // port of client application. MAKE SURE THAT THIS PORT IS FREE AND CLIENT PORT DOES NOT MATCH SERVER PORT
     },
 
     system: { // advanced settings, do not edit
-        cwd: process.cwd() // globalsDB directory fix
+        cwd: process.cwd() // globalsDB directory change fix
     }
 
 };
