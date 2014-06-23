bbad7ca02c13cb7eff2a1497ebeb02b7fdc23531
ZitRos <zitros.lab@gmail.com>
2014-06-23 23:59:35 +0300

    comments changes, GlobalsDB add-on files fixes

--- config.js
+++ config.js
@@ -1,29 +1,29 @@
 /**
  * That's the global configuration file for application. Feel free to edit it, but do not forget
- * to make reserve copy to restore default settings.
+ * to hold a reserve copy of it to restore default settings.
  */
 module.exports = {
 
     database: { // database settings
-        modulePath: "globalsDB/cache", // globalsDB path to node module file (without extension)
+        modulePath: "globalsDB/cache", // path to node addon file (./node_modules/<modulePath>.node)
         databases: { // list of available database directories
-            primaryDB: "C:/HardProgram/GlobalsDB/mgr", // name: "path", name2: "path2", ...
-            Cache: "C:/HardProgram/Cache/mgr"
+            GlobalsDB: "C:/HardProgram/GlobalsDB/mgr", // name: "path", name2: "path2", ...
+            Cache: "C:/HardProgram/Cache/mgr" // replace with "/" or escape "\" characters in path
         }
     },
 
     server: { // server settings
         port: 57775, // port of server WebSocket application
-        masterPassword: "protect", // security password
+        masterPassword: "protect", // authorisation password
         useMasterPassword: true, // turn master password usage on
         log: true, // log connections
-        logFileName: "log.txt" // !RELATIVE! path with file name
+        logFileName: "log.txt" // relative to this folder: path to log file including file name
     },
 
     localClient: { // local client settings
         enabled: true, // use local HTTP client (for browser application)
-        directory: "client", // !RELATIVE! frontend location to this file
-        port: 80 // port of client application. MAKE SURE THAT THIS PORT IS FREE AND CLIENT PORT DOES NOT MATCH SERVER PORT
+        directory: "client", // location of shared directory via HTTP (local client)
+        port: 80 // port of client application. The value must not be equal to server port value
     }
 
 };
